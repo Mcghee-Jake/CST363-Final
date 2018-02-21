aa8d405a658d12dcc8375d098baa89462e4c68e2
McGhee-Jake <jmcghee@csumb.edu>
2018-02-20 22:58:06 -0800

    switched all views to the views file

--- view.sql
+++ view.sql
@@ -18,3 +18,40 @@ ORDER BY a.animal_id;
 
 -- This select statement just to test the above view out.
 SELECT * FROM all_animal_info_view;
+ 
+-- VIEWS to show show the names and species of all the animals in a particular exhibit
+CREATE VIEW exhibit1_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 1;
+
+CREATE VIEW exhibit2_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 2;
+
+CREATE VIEW exhibit3_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 3;
+
+CREATE VIEW exhibit4_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 4;
+
+CREATE VIEW exhibit5_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 5;
+
+CREATE VIEW exhibit6_view AS
+SELECT animal_name, sex, dob, common_name, science_name
+FROM animal a 
+JOIN species s ON a.species_id = s.species_id
+WHERE exhibit_id = 6;
--- zooSelects.sql
+++ zooSelects.sql
@@ -31,13 +31,6 @@ SELECT * FROM species;
 SELECT * FROM natural_habitat;
 SELECT * FROM species_has_natural_habitat;
 
--- SELECT to see general animal information
-SELECT a.animal_id, a.animal_name, a.dob, a.sex, e.exhibit_name, s.common_name, s.science_name, s.endangered
-FROM animal a
-JOIN exhibit e ON e.exhibit_id = a.exhibit_id
-JOIN species s ON s.species_id = a.species_id
-ORDER BY a.animal_id;
-
 -- SELECT to see all the species and their corresponding habitats 
 -- NOTE You will see duplicate records, but that is ok to denormalize here because the zookeeper wants to 
 -- go down alphabetically by species and see all the records for what habitats they live in. 
@@ -137,57 +130,4 @@ ELSE 'False' END AS 'Female animals > Male animals'
   FROM animal;
   
 
--- VIEWS--------------------------------------------------------------------------
--- --------------------------------------------------------------------------  
-
--- This View will allow a user to more easily many different attributes of an animal
--- It can be used to display all the animals in the Zoo with information about them from several different tables
--- It may be best to use something like this for the web views
-CREATE OR REPLACE VIEW all_animal_info_view
-AS
-SELECT a.animal_id, a.animal_name, a.dob, 
-a.sex, e.exhibit_name, s.common_name, 
-s.science_name, IF(s.endangered,'True','False') As Endangered
-FROM animal a
-JOIN exhibit e ON e.exhibit_id = a.exhibit_id
-JOIN species s ON s.species_id = a.species_id
-ORDER BY a.animal_id;  
-  
- 
--- VIEWS to show show the names and species of all the animals in a particular exhibit
-CREATE VIEW exhibit1_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 1;
-
-CREATE VIEW exhibit2_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 2;
-
-CREATE VIEW exhibit3_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 3;
-
-CREATE VIEW exhibit4_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 4;
-
-CREATE VIEW exhibit5_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 5;
-
-CREATE VIEW exhibit6_view AS
-SELECT animal_name, sex, dob, common_name, science_name
-FROM animal a 
-JOIN species s ON a.species_id = s.species_id
-WHERE exhibit_id = 6;
 
